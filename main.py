0bb4875cd991cad740c126e503ad881a710d3cb4
Dawid Smulkowski <dawidsmulkowski@gmail.com>
2020-03-22 19:03:41 +0100

    Add multithreading, change browser to chrome, add execution time

--- main.py
+++ main.py
@@ -1,39 +1,57 @@
+from threading import Thread
+from datetime import datetime
+from queue import Queue
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import json
 
-
+'''
+TODO:
+Add async (actually learn how to execute it in thread)
+Add config file
+Add list of product to search
+'''
 def main():
+    product_name = input("Podaj interesujacy cie produkt: ")
+    threads = []
+    queue = Queue()
+    start = datetime.now()
+    browser_options = webdriver.ChromeOptions()
+    browser_options.add_argument('headless')
     with open('shops_structure.json') as f:
         shops_structure = json.load(f)
 
     with open('shops_info.json') as f:
         shop_info = json.load(f)
 
+    #products_list = {}
+    for i, key in enumerate(shops_structure.keys()):
+        threads.append(Thread(target=get_products,args=(shops_structure, shop_info, product_name, key, browser_options, queue), daemon=True))
+        threads[i].start()
 
-    product_name = input("Podaj interesujacy cie produkt: ")
-    driver = webdriver.PhantomJS('/home/skav/Pobrane/phantomjs-2.1.1-linux-x86_64/bin/phantomjs')
-    driver.set_window_size(1120, 550)
-    products_list = get_products(shops_structure, shop_info, product_name, driver)
-    print(products_list)
+    for thread in threads:
+        thread.join()
+
+    print('execution time:', datetime.now()-start)
+    print(queue.queue)
 
 
-def get_products(shops_structure, shop_info, product_name, driver):
-    shops_products={}
-    for key in shops_structure.keys():
-        product_dict = {}
+def get_products(shops_structure, shop_info, product_name, key, browser_options,queue):
+        print(key)
         shop_struct = shops_structure[key]
         product_name_keys = product_name.lower().split(" ")
         product_separated_name = product_name.replace(" ", shop_info[key]["separator"])
         url = "{}{}".format(shop_info[key]["url"], product_separated_name)
+        product_dict = {}
 
+        driver = webdriver.Chrome(options=browser_options)
         driver.get(url)
         soup = BeautifulSoup(driver.page_source, 'html.parser')
 
         products_list = soup.find(shop_struct["all_products_container"]["type"], attrs=shop_struct["all_products_container"]["attrs"])
 
         if not products_list:
-            continue
+            return None
 
         products = products_list.find_all_next(shop_struct["products_container"]["type"], attrs=shop_struct["products_container"]["attrs"])
 
@@ -42,12 +60,11 @@ def get_products(shops_structure, shop_info, product_name, driver):
             if not name:
                 continue
 
-            price = get_price(product, shop_struct)
             link = get_link(product, shop_struct)
+            price = get_price(product, shop_struct)
 
-            product_dict[name] = {'price': price["price"], 'discount_price': price["discount_price"], 'link': link}
-        shops_products[key] = product_dict
-    return shops_products
+            product_dict[name] = {'price': price["regular"], 'discount_price': price["discount"], 'link': link}
+        queue.put(product_dict)
 
 def get_name(product, product_name_keys, shop_struct):
     name_container = product.find(shop_struct["product_name"]["type"], attrs=shop_struct["product_name"]["attrs"])
@@ -61,7 +78,6 @@ def get_name(product, product_name_keys, shop_struct):
     if not all(x in name.lower() for x in product_name_keys):
         return False
     return name
-
 
 def get_price(product, shop_struct):
     discount_price = "0"
@@ -94,7 +110,7 @@ def get_price(product, shop_struct):
                 discount_price = product.find(shop_struct["product_discount_price"]["type"],
                                               attrs=shop_struct["product_discount_price"]["attrs"]).text
 
-    return {"price": regular_price, "discount_price": discount_price}
+    return {"regular": regular_price, "discount": discount_price}
 
 
 def get_link(product, shop_struct):
